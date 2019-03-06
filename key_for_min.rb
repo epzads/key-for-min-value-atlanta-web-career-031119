5f105cc6ef6d3a56ced0b2e878936c210f419ff5
favorable-vector-1743 <epzads@gmail.com>
2019-03-06 02:30:25 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,6 +2,23 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
+hash = {:blake => 500, :ashley => 2, :adam => 10}
+
+lowvalue = Float::INFINITY
+newkey = 0
+
+ 
+hash.collect do |k,v|
+
+
+
+ if lowvalue > v
+    lowvalue = v 
+     newkey = k  
+   
+ end 
+  
+end 
 
 
 
