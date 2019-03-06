9bb02ed0d74a80ba1151c6decae775a5a5e115f6
favorable-vector-1743 <epzads@gmail.com>
2019-03-06 02:33:16 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -2,7 +2,7 @@
 # Given a hash with numeric values, return the key for the smallest value
 
 def key_for_min_value(name_hash)
-hash = {:blake => 500, :ashley => 2, :adam => 10}
+hash = {:blake => 500, :ashley => 2, :adam => 1}
 
 lowvalue = 600000
 newkey = 0
